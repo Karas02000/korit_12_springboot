bd6d6af14346ce1f5e741482af15df0fba5e5782
Karas02000 <gta5369577@naver.com>
2026-02-23 11:23:43 +0900

    2026/02/23 | 11:23

--- demo/src/main/java/com/example/demo/domain/Owner.java
+++ demo/src/main/java/com/example/demo/domain/Owner.java
@@ -1,13 +1,54 @@
 package com.example.demo.domain;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.lang.NonNull;
 
+import java.util.List;
+
+@Entity
 public class Owner {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private String firstName;
-    private String lastName;
+    private long ownerId;
+    @NonNull
+    private String firstName, lastName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private List<Car> cars;
+
+    public Owner() {}
+    public Owner(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    @NonNull
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@NonNull String firstName) {
+        this.firstName = firstName;
+    }
+
+    @NonNull
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@NonNull String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
 }
